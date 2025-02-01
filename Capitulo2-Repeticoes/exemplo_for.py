77371ddd65ca82ddd3bdd9fbda8f3b2eded6822a
Kepler Medeiros <kepler_medeiros@hotmail.com>
2025-01-31 23:58:21 -0300

    Primeiro Commit

---
+++ Capitulo2-Repeticoes/exemplo_for.py
@@ -0,0 +1,6 @@
+for numero in range (1, int(input('Digite um número para determinar o fim: ')), 1):
+    print('\t' + str(numero))
+
+# Range especifica uma faixa de valores e como será incrementada.
+# A variável iniciará com o valor 1 até o valor que for digitado pelo usuário.
+# A faixa será incrementada de 1 em 1, conforme o último valor especificado no range
